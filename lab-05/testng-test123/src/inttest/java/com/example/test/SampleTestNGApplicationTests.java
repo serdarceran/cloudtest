fd52c4adbfd4cc4e73105ff17bf8e8108ea37085
Serdar CERAN <ceranbs@gmail.com>
2017-02-01 01:06:40 +0300

    initial commit

--- lab-05/testng-test123/src/inttest/java/com/example/test/SampleTestNGApplicationTests.java
+++ lab-05/testng-test123/src/inttest/java/com/example/test/SampleTestNGApplicationTests.java
@@ -1,20 +1,29 @@
-package com.example.s1;
+package com.example.test;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @DirtiesContext
 public class SampleTestNGApplicationTests extends AbstractTestNGSpringContextTests {
 
-    @Test
-    @Parameters({"dbconfig", "poolsize"})
-    public void createConnection(String dbconfig, int poolsize) {
-        System.out.println("dbconfig : " + dbconfig);
-        System.out.println("poolsize : " + poolsize);
+    @Test(groups = { "GroupSAJ"})
+    public void testForSAJ() {
+        System.out.println(">>>>>>>>>>>>>>>>Test Executed for SAJ");
     }
+
+    @Test(groups = { "GroupSAL"})
+    public void testForSAL() {
+        System.out.println(">>>>>>>>>>>>>>>>>>>Test Executed for SAL");
+    }
+
+    @Test(groups = { "GroupSAJ", "GroupSAL"})
+    public void testForSALandSAJ() {
+        System.out.println(">>>>>>>>>>>>>>>>Test Executed for SAL and SAJ");
+    }
+
+
 }
